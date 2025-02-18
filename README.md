eb09555707d383228b19d50cb6122cc5ad331175
Priyanshu Sharma <18priyanshusharma@gmail.com>
2025-02-18 22:37:02 +0530

    Initial commit git rebase --continue
    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Portfolio
+
+I am Priyanshu
---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Priyanshu Sharma </span>
+            from <span className="purple"> Bihar, India.</span>
+            <br />
+            I am an ex- ML intern at Infosys.
+            <br />
+            I am currently a final year B.Tech CSE student at Vellore Institute of Technology (VIT)
+            .
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Research Activity
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writing Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Priyanshu</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
